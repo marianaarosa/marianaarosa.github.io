91d5c1612a27a7cfdbc5b8adf997c127f8bba4d0
marianaarosa <marianaarosa88@gmail.com>
2023-04-26 11:53:02 -0400

    funçao da pagina inicial

---
+++ function.js
@@ -0,0 +1,15 @@
+const title = "Projetos GitHub @marianaarosa";
+
+function typeWriter(element, text, speed) {
+  let i = 0;
+  const interval = setInterval(() => {
+    element.innerHTML += text.charAt(i);
+    i++;
+    if (i > text.length) clearInterval(interval);
+  }, speed);
+}
+
+window.addEventListener("load", () => {
+  const titleElement = document.getElementById("typing");
+  typeWriter(titleElement, title, 100);
+});
